53d99d07617a822d0d266249f0ba3fdbef1c42dc
Kiruba C <154721675+2812kiruba@users.noreply.github.com>
2024-08-10 12:13:45 +0530

    Add files via upload

---
+++ AssemblyAIApiClient.cs
@@ -0,0 +1,67 @@
+﻿using AssemblyAITranscriber.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AssemblyAITranscriber
+{
+ public class AssemblyAIApiClient
+    {
+        private readonly string _apiToken;
+        private readonly string _baseUrl;
+        private readonly HttpClient _httpClient;
+
+        public AssemblyAIApiClient(string apiToken, string baseUrl)
+        {
+            _apiToken = apiToken;
+            _baseUrl = baseUrl;
+            _httpClient = new HttpClient() { BaseAddress = new Uri(_baseUrl) };
+            _httpClient.DefaultRequestHeaders.Add("Authorization", _apiToken);
+        }
+
+        private async Task<TModel> SendRequestAsync<TModel>(HttpRequestMessage request)
+        {
+            var response = await _httpClient.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<TModel>(json);
+        }
+        public Task<UploadAudioResponse> UploadFileAsync(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException(filePath);
+            }
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "v2/upload");
+            request.Headers.Add("Transer-Encoding", "chunked");
+
+            var fileReader = File.OpenRead(filePath);
+            request.Content = new StreamContent(fileReader);
+
+            return SendRequestAsync<UploadAudioResponse>(request);
+        }
+        public Task<TranscriptionResponse> SubmitAudioFileAsync(string audioUrl)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "v2/transcript");
+            var requestBody = JsonSerializer.Serialize(new TranscriptionRequest { AudioUrl = audioUrl });
+            request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            return SendRequestAsync<TranscriptionResponse>(request);
+        }
+
+        public Task<TranscriptionResponse> GetTranscriptionAsync(string id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"v2/transcript/{id}");
+
+            return SendRequestAsync<TranscriptionResponse>(request);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using AssemblyAITranscriber.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AssemblyAITranscriber
+{
+    internal class Program
+    {
+        private const string API_KEY = "75b5f6fe188e42d28a8d0d70f0e5b4fb";
+        private const string API_URL = "https://api.assemblyai.com/";
+        //private const string AUDIO_FILE_PATH = "./audio/CallRecording4.mp3";
+        private const string AUDIO_FILE_PATH = "C:\\Users\\rndbcp\\source\\repos\\AssemblyAITranscriber\\audio\\CallRecording4.mp3";
+        static void Main(string[] args)
+        {
+            var client = new AssemblyAIApiClient(API_KEY, API_URL);
+
+            // Upload file
+            var uploadResult = client.UploadFileAsync(AUDIO_FILE_PATH).GetAwaiter().GetResult();
+
+            // Submit file for transcription
+            var submissionResult = client.SubmitAudioFileAsync(uploadResult.UploadUrl).GetAwaiter().GetResult();
+            Console.WriteLine($"File {submissionResult.Id} in status {submissionResult.Status}");
+
+            // Query status of transcription until it's `completed`
+            TranscriptionResponse result = client.GetTranscriptionAsync(submissionResult.Id).GetAwaiter().GetResult();
+            while (!result.Status.Equals("completed"))
+            {
+                Console.WriteLine($"File {result.Id} in status {result.Status}");
+                Thread.Sleep(15000);
+                result = client.GetTranscriptionAsync(submissionResult.Id).GetAwaiter().GetResult();
+            }
+
+            // Perform post-procesing with the result of the transcription
+            Console.WriteLine($"File {result.Id} in status {result.Status}");
+            Console.WriteLine($"{result.Words?.Count} words transcribed.");
+
+            foreach (var word in result.Words)
+            {
+                Console.Write(word.Text+" ");
+              //  Console.WriteLine($"Word: '{word.Text}' at {word.Start} with {word.Confidence * 100}% confidence.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
