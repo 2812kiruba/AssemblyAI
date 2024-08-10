90562453eb5cc51a35038f3da3ea577ff5cd30ce
Kiruba C <154721675+2812kiruba@users.noreply.github.com>
2024-08-10 12:18:26 +0530

    Add files via upload

---
+++ Model/UploadAudioResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+
+namespace AssemblyAITranscriber.Model
+{
+    public class UploadAudioResponse
+    {
+        [JsonPropertyName("upload_url")]
+        public string UploadUrl { get; set; }
+    }
+}
---
+++ Model/Word.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AssemblyAITranscriber.Model
+{
+   public class Word
+    {
+        [JsonPropertyName("confidence")]
+        public double Confidence { get; set; }
+
+        [JsonPropertyName("end")]
+        public int End { get; set; }
+
+        [JsonPropertyName("start")]
+        public int Start { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+}
